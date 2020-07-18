78854eaad597018bc6f56b8c419a848946bc8d06
Vishnu Mohan G <mail@vishnu.pw>
2020-07-18 09:25:46 +0530

    beautify

--- src/Board/Board.js
+++ src/Board/Board.js
@@ -72,7 +72,10 @@ export default class Board extends Component {
         }
 
         return (
-            <div>
+            <div style={{
+                position: 'absolute', left: '50%', top: '50%',
+                transform: 'translate(-50%, -50%)'
+            }}>
                 { boardElements}
             </div>
         )
--- src/Board/Cell.js
+++ src/Board/Cell.js
@@ -8,7 +8,7 @@ function cell(props) {
     const imgs = [blankImg, xImg, oImg]
     const img = imgs[props.val||0]
     return (
-        <img onClick={props.onClick.bind(this, props.row, props.col, props.val)} src={img} height="100px" border='1px'/>
+        <img onClick={props.onClick.bind(this, props.row, props.col, props.val)} src={img} height="75px" border='1px'/>
     )
 }
 
